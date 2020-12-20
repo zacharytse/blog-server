c92730cbb2b6e71b99afcf678282d8c1c7f3a9b7
xie2481 <5396985+chaoqiangxie@user.noreply.gitee.com>
2020-12-20 18:25:16 +0800

    ignored file

---
+++ blog-gateway/src/main/java/com/xcq/bloggateway/BlogGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.xcq.bloggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BlogGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogGatewayApplication.class, args);
+    }
+
+}
---
+++ eureka-server/src/main/java/com/xcq/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.xcq.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册服务中心
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
